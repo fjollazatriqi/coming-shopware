397e118253b70697af1ed39d14984c5972b90bc1
Fjolla Zatriqi <fjollazatriqi97@gmail.com>
2025-11-24 20:52:45 +0100

    Initial commit – Shopware 6 project setup

---
+++ custom/plugins/ComingTheme/src/ComingTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ComingTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ComingTheme extends Plugin implements ThemeInterface
+{
+}
